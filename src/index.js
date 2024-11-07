2a8dd1ada3c444999da471989ac1225d4f257444
Hamzaalsafi <hamzasf345@gmail.com>
2024-11-07 22:39:51 +0300

    start to share

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { StudentProvider } from './StudentContext'; 
+import { Analytics } from "@vercel/analytics/react"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <StudentProvider>
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
   </StudentProvider>
 );
